69660b6a006aecbd84cbd3396d03e6cb9b90a510
BALKNAoppa <baterdene207@gmail.com>
2025-01-23 10:24:23 +0800

    input updates

--- src/components/StepOne.js
+++ src/components/StepOne.js
@@ -2,68 +2,112 @@ import React from "react";
 import PineconeLogo from "@/icons/PineconeLogo";
 import { isStepOneValid } from "@/utils/stepOneValidation";
 const StepOne = (props) => {
-    const { handleNextStep, errors, formValue, handleError, setFormValue, clearError } = props;
+  const {
+    handleNextStep,
+    errors,
+    formValue,
+    handleError,
+    setFormValue,
+    clearError,
+  } = props;
 
-    const handleChange = (event) => {
-        const { name, value } = event.target;
-        setFormValue((prev) => ({
-            ...prev,
-            [name]: value,
-        }));
-        clearError(name);
-    };
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormValue((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+    clearError(name);
+  };
 
-    const handleFormNextStep = () => {
-        const { isValid, errors } = isStepOneValid(formValue)
+  const handleFormNextStep = () => {
+    const { isValid, errors } = isStepOneValid(formValue);
 
-        if (isValid) {
-            handleNextStep();
-        }
-        handleError(errors);
-    };
-    
-    return (
-        <div class="w-screen h-screen flex items-center justify-center bg-gray-100">
-            <div class="w-[480px] h-[655px] rounded-lg p-8 bg-white flex flex-col justify-between">
-                <div class="w-[416px] h-[385px]">
-                    <div class="w-[416px] h-[129px] flex flex-col justify-between">
-                        <PineconeLogo />
-                        <p class="w-[416px] h-[31px] font-manrope text-[26px] font-semibold leading-[31.47px] tracking[-0.03em] decoration-skip-[ink]">Join Us! 😎</p>
-                        <p class="w-[416px] h-[22px] text-gray-500 font-manrope text-[18px] font-normal leading-[21.78px]">Please provide all current information accurately.</p>
-                    </div>
-                    <div class="w-[416px] h-fit gap-3">
-                        <div class="w-[426px] h-[68px] gap-2 flex flex-col">
-                            <label class="font-manrope text-sm font-semibold leading-4 tracking-tight text-lef" >
-                                Firstname <span class="text-red-500">*</span>
-                            </label>
-                            <input placeholder="Placeholder" name={"firstName"} class={`${errors.firstName.length > 0 ? "" : ""}, border rounded w-[416px] h-[44px] p-1 focus:border-blue-500`}
-                                onChange={handleChange}
-                            />
-                            {errors.firstName.length > 0 && <p class="text-red-500" >{errors.firstName}</p>}
-                        </div>
-                        <div class=" w-[426px] h-[68px] gap-2 flex flex-col">
-                            <label class="font-manrope text-sm font-semibold leading-4 tracking-tight text-lef">
-                                Lastname <span class="text-red-500">*</span>
-                            </label>
-                            <input placeholder="Placeholder" name={"firstName"} class={`${errors.firstName.length > 0 ? "" : ""}, border rounded w-[416px] h-[44px] p-1 focus:border-blue-500`}
-                                onChange={handleChange}
-                            />
-                            {errors.firstName.length > 0 && <p class="text-red-500" >{errors.firstName}</p>}
-                        </div>
-                        <div class="w-[416px] h-[68px] gap-2">
-                            <label class="font-manrope text-sm font-semibold leading-4 tracking-tight text-lef" >
-                                Username <span class="text-red-500">*</span>
-                            </label>
-                            <input placeholder="Placeholder" name={"firstName"} class={`${errors.firstName.length > 0 ? "" : ""}, border rounded w-[416px] h-[44px] p-1 focus:border-blue-500`}
-                                onChange={handleChange}
-                            />
-                            {errors.firstName.length > 0 && <p class="text-red-500" >{errors.firstName}</p>}
-                        </div>
-                    </div>
-                </div>
-                <button class="w-[416px] h-[44px] bg-black  text-white rounded-lg " onClick={handleFormNextStep} > Continue 1/3 </button>
+    if (isValid) {
+      handleNextStep();
+    }
+    handleError(errors);
+  };
+
+  return (
+    <div className="w-screen h-screen flex items-center justify-center bg-gray-100">
+      <div className="w-[480px] h-[655px] rounded-lg p-8 bg-white flex flex-col justify-between">
+        <div className="w-full mx-1">
+          <div className="w-full flex flex-col justify-between mb-7">
+            <PineconeLogo />
+            <p className="w-full  font-manrope text-[26px] font-semibold leading-[31.47px] tracking[-0.03em] decoration-skip-[ink]">
+              Join Us! 😎
+            </p>
+            <p className="w-full  text-gray-500 font-manrope text-[18px] font-normal leading-[21.78px]">
+              Please provide all current information accurately.
+            </p>
+          </div>
+
+
+          <div className="w-full flex flex-col justify-between my-2">
+
+            <div className="w-full gap-2 flex flex-col mb-3">
+              <label className="font-manrope text-sm font-semibold leading-4 tracking-tight text-lef">
+                Firstname <span className="text-red-500">*</span>
+              </label>
+              <input
+                placeholder="Placeholder"
+                name={"firstName"}
+                className={`${
+                  errors.firstName.length > 0 ? "" : ""
+                }, border rounded-lg w-full p-2 focus:border-blue-500`}
+                onChange={handleChange}
+              />
+              {errors.firstName.length > 0 && (
+                <p className="text-red-500">{errors.firstName}</p>
+              )}
             </div>
+
+
+            <div className=" w-full gap-2 flex flex-col mb-3">
+              <label className="font-manrope text-sm font-semibold leading-4 tracking-tight text-lef">
+                Lastname <span className="text-red-500">*</span>
+              </label>
+              <input
+                placeholder="Placeholder"
+                name={"firstName"}
+                className={`${
+                  errors.firstName.length > 0 ? "" : ""
+                }, border rounded-lg w-full p-2 focus:border-blue-500`}
+                onChange={handleChange}
+              />
+              {errors.firstName.length > 0 && (
+                <p className="text-red-500">{errors.firstName}</p>
+              )}
+            </div>
+
+            <div className="w-full gap-2 flex flex-col mb-3">
+              <label className="font-manrope text-sm font-semibold leading-4 tracking-tight text-lef">
+                Username <span className="text-red-500">*</span>
+              </label>
+              <input
+                placeholder="Placeholder"
+                name={"firstName"}
+                className={`${
+                  errors.firstName.length > 0 ? "" : ""
+                }, border rounded-lg w-full p-2 focus:border-blue-500`}
+                onChange={handleChange}
+              />
+              {errors.firstName.length > 0 && (
+                <p className="text-red-500">{errors.firstName}</p>
+              )}
+            </div>
+          </div>
         </div>
-    )
+        <button
+          className="w-full bg-black  text-white rounded-lg py-3 "
+          onClick={handleFormNextStep}
+        >
+          {" "}
+          Continue 1/3{" "}
+        </button>
+      </div>
+    </div>
+  );
 };
 export default StepOne;
--- src/components/StepThree.js
+++ src/components/StepThree.js
@@ -6,18 +6,18 @@ const StepThree = (props) => {
     const { handleNextStep, handleBackStep } = props;
 
     return (
-        <div class="w-screen h-screen flex items-center justify-center bg-gray-100">
-            <div class="w-[480px] h-[655px] rounded-lg p-8 bg-white flex flex-col justify-between">
-                <div class="w-[416px] h-[385px]">
-                    <div class="w-[416px] h-[129px] flex flex-col justify-between">
+        <div className="w-screen h-screen flex items-center justify-center bg-gray-100">
+            <div className="w-[480px] h-[655px] rounded-lg p-8 bg-white flex flex-col justify-between">
+                <div className="w-[416px] h-[385px]">
+                    <div className="w-[416px] h-[129px] flex flex-col justify-between">
                         <PineconeLogo />
-                        <p class="w-[416px] h-[31px] font-inter text-[26px] font-semibold leading-[31.47px] tracking[-0.03em] decoration-skip-[ink]">Step 3 😎</p>
-                        <p class="w-[416px] h-[22px] text-gray-500 font-inter text-[18px] font-normal leading-[21.78px]">Please provide all current information accurately.</p>
+                        <p className="w-[416px] h-[31px] font-inter text-[26px] font-semibold leading-[31.47px] tracking[-0.03em] decoration-skip-[ink]">Step 3 😎</p>
+                        <p className="w-[416px] h-[22px] text-gray-500 font-inter text-[18px] font-normal leading-[21.78px]">Please provide all current information accurately.</p>
                     </div>
                 </div>
-                <div class="flex justify-between">
-                    <button class="w-[128px] h-[44px] bg-white  text-black rounded-lg border border-[rgba(203,213,225,1)] " onClick={handleBackStep} > <span>&lt;</span> Back</button>
-                    <button class="w-[280px] h-[44px] bg-black  text-white rounded-lg " onClick={handleNextStep} > Continue 3/3 <span>&gt;</span></button>
+                <div className="flex justify-between">
+                    <button className="w-[128px] h-[44px] bg-white  text-black rounded-lg border border-[rgba(203,213,225,1)] " onClick={handleBackStep} > <span>&lt;</span> Back</button>
+                    <button className="w-[280px] h-[44px] bg-black  text-white rounded-lg " onClick={handleNextStep} > Continue 3/3 <span>&gt;</span></button>
                 </div>
             </div>
         </div>
